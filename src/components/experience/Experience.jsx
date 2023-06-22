750ddce9eca2bb876a71e76bdcaf61c19a43fcb7
Edwin M <edwinjmenendez@gmail.com>
2023-06-22 02:09:49 -0700

    finished experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,6 +4,40 @@ import './Experience.css'
  
 const Experience = () => {
 
+  const fontendSkills = [
+    { name: 'JavaScript', level: 'Proficient' },
+    { name: 'TypeScript', level: 'Proficient' },
+    { name: 'ReactJS', level: 'Proficient' },
+    { name: 'HTML5', level: 'Proficient' },
+    { name: 'CSS3', level: 'Proficient' },
+    { name: 'Webpack', level: 'Proficient' },
+    { name: 'AngularJS', level: 'Proficient' }, 
+    { name: 'React Native', level: 'Experienced' },
+    { name: 'jQuery', level: 'Experienced' },
+    { name: 'Jest', level: 'Experienced' },
+    { name: 'Enzyme', level: 'Experienced' },
+  ];
+
+  const backendSkills = [
+    
+    { name: 'Python', level: 'Proficient' },
+    { name: 'SQL', level: 'Proficient' },
+    { name: 'PostgreSQL', level: 'Proficient' },
+    { name: 'Athena', level: 'Proficient' },
+    { name: 'Node.js', level: 'Experienced' },
+    { name: 'Express', level: 'Experienced' },
+    { name: 'Pytest', level: 'Experienced' },
+    { name: 'AWS CDK', level: 'Experienced' },
+    { name: 'Glue', level: 'Experienced' },
+    { name: 'DynamoDB', level: 'Experienced' },
+    { name: 'AWS Lambda', level: 'Experienced' },
+    { name: 'CloudFormation', level: 'Experienced' },
+    { name: 'S3', level: 'Experienced' },
+    { name: 'Docker', level: 'Experienced' },
+    { name: 'NoSQL', level: 'Experienced' },
+    { name: 'MongoDB', level: 'Experienced' },
+  ];
+
   return (
     <section id='experience'>
       <h5>Skills I have</h5>
@@ -13,17 +47,29 @@ const Experience = () => {
         <div className="experience_frontend">
           <h3>Frontend Development</h3>
           <div className="experience_content">
-            <article className='experience_details'>
-              <BsFillPatchCheckFill />
-              <h4>HTML</h4>
-              <small className='text-light'>Experienced</small>
-            </article>
+            {fontendSkills.map(({level, name}) => (
+              <article className='experience_details'>
+                <BsFillPatchCheckFill className='experience_details-icon' />
+                <div>
+                  <h4>{name}</h4>
+                  <small className='text-light'>{level}</small>
+                </div>
+              </article>
+            ))}
           </div>
         </div>
         <div className="experience_backend">
           <h3>Backend Development</h3>
             <div className="experience_content">
-              <article className='experience_details' ></article>
+              {backendSkills.map(({ level, name }) => (
+                <article className='experience_details'>
+                  <BsFillPatchCheckFill className='experience_details-icon' />
+                  <div>
+                    <h4>{name}</h4>
+                    <small className='text-light'>{level}</small>
+                  </div>
+                </article>
+              ))}
             </div>
           </div>
       </div>
